b96679a8449e683d8702fa4ce5e143f6a0ca63fa
Luan Ricardo <luanrnr1227@gmail.com>
2024-05-09 17:06:11 -0300

    New features - Homepage

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -3,145 +3,117 @@
 /*==================== CHANGE BACKGROUND HEADER ====================*/
 
 //Loader
-document
-  .addEventListener("DOMContentLoaded", () => {
-    const tl = gsap.timeline();
-    tl.fromTo("body", 1, { scrollToTop }, { scrollToTop }, "-=1.5")
+document.addEventListener("DOMContentLoaded", () => {
+  const tl = gsap.timeline();
 
-      .fromTo(
-        ".loader",
-        1,
-        { width: "100%" },
-        { width: "0%", delay: 4, ease: Expo.easeInOut }
-      )
+  tl.fromTo("body", 1.5, { scrollToTop }, { scrollToTop }, "-=1.5");
 
-      .fromTo("body", 1, { overflow: "hidden" }, { overflow: "auto" }, "-=1")
-
-      .fromTo(
-        ".delay-bg",
-        1.5,
-
-        { y: 50, opacity: 0 },
-        { y: 0, opacity: 1, ease: Expo.easeInOut },
-        "-=0.1"
-      )
-      .fromTo(
-        ".top",
-        0.1,
-        { opacity: 0 },
-        { opacity: 1, ease: Expo.easeInOut },
-        "-=0.1"
-      )
-      .fromTo(
-        ".show",
-        0.5,
-        { y: 50, opacity: 0 },
-        { y: 0, opacity: 1, ease: Expo.ease },
-        "-=0.5"
-      )
-
-      .fromTo(
-        ".info-title",
-        0.1,
-        { y: 50, opacity: 0 },
-        { y: 0, opacity: 1, ease: Expo.ease },
-        "-=1"
-      )
-      .fromTo(
-        ".title",
-        0.1,
-        { y: 50, opacity: 0 },
-        { y: 0, opacity: 1, ease: Expo.ease },
-        "-=1.2"
-      )
-      .fromTo(
-        ".dc",
-        0.1,
-        { y: 50, opacity: 0 },
-        { y: 0, opacity: 1, ease: Expo.ease },
-        "-=0.01"
-      )
-      .fromTo(
-        ".dv",
-        0.1,
-        { y: 50, opacity: 0 },
-        { y: 0, opacity: 1, ease: Expo.ease },
-        "-=0.01"
-      )
-
-      .fromTo(
-        ".info-title2",
-        0.01,
-        { y: 50, opacity: 0 },
-        { y: 0, opacity: 1, ease: Expo.ease },
-        "-=0.5"
-      )
-      .fromTo(
-        ".title2",
-        0.01,
-        { y: 50, opacity: 0 },
-        { y: 0, opacity: 1, ease: Expo.ease },
-        "-=0.05"
-      )
-      .fromTo(
-        ".loc",
-        0.01,
-        { y: 50, opacity: 0 },
-        { y: 0, opacity: 1, ease: Expo.ease },
-        "-=0.001"
-      )
-
-      .fromTo(
-        ".navbar",
-        3,
-        { opacity: 0 },
-        { opacity: 1, ease: Expo.easeInOut },
-        "-=0.01"
-      );
-  })
-  .from(
-    ".container-hero .grid-7 div",
-    {
-      duration: 2,
-      opacity: 0,
-      y: 200,
-      stagger: { each: 0.1 },
-      ease: "power4.out",
-    },
-    "-=1.9"
+  tl.fromTo(
+    ".loader",
+    1,
+    { width: "100%" },
+    { width: "0%", delay: 4, ease: Expo.easeInOut }
   )
-  .from(
-    ".container-hero .grid-5 div",
-    {
-      duration: 2,
-      opacity: 0,
-      y: 200,
-      stagger: { each: 0.1 },
-      ease: "power4.out",
+
+    .fromTo("body", 1, { overflow: "hidden" }, { overflow: "auto" }, "-=1")
+
+    .fromTo(
+      ".delay-bg",
+      1.5,
+
+      { y: 50, opacity: 0 },
+      { y: 0, opacity: 1, ease: Expo.easeInOut },
+      "-=0.1"
+    )
+    .fromTo(
+      ".top",
+      0.1,
+      { opacity: 0 },
+      { opacity: 1, ease: Expo.easeInOut },
+      "-=0.1"
+    )
+
+    .fromTo(
+      ".info-title",
+      0.1,
+      { y: 50, opacity: 0 },
+      { y: 0, opacity: 1, ease: Expo.ease },
+      "-=1"
+    )
+    .fromTo(
+      ".title",
+      0.1,
+      { y: 50, opacity: 0 },
+      { y: 0, opacity: 1, ease: Expo.ease },
+      "-=1.2"
+    )
+    .fromTo(
+      ".dc",
+      0.1,
+      { y: 50, opacity: 0 },
+      { y: 0, opacity: 1, ease: Expo.ease },
+      "-=0.01"
+    )
+    .fromTo(
+      ".dv",
+      0.1,
+      { y: 50, opacity: 0 },
+      { y: 0, opacity: 1, ease: Expo.ease },
+      "-=0.01"
+    )
+
+    .fromTo(
+      ".info-title2",
+      0.01,
+      { y: 50, opacity: 0 },
+      { y: 0, opacity: 1, ease: Expo.ease },
+      "-=0.5"
+    )
+    .fromTo(
+      ".title2",
+      0.01,
+      { y: 50, opacity: 0 },
+      { y: 0, opacity: 1, ease: Expo.ease },
+      "-=0.05"
+    )
+    .fromTo(
+      ".loc",
+      0.01,
+      { y: 50, opacity: 0 },
+      { y: 0, opacity: 1, ease: Expo.ease },
+      "-=0.001"
+    )
+
+    .fromTo(
+      ".navbar",
+      3,
+      { opacity: 0 },
+      { opacity: 1, ease: Expo.easeInOut },
+      "-=0.01"
+    );
+
+  // ANIMAÇÃO DAS 2 FRASES
+  gsap.from("#text-creative", {
+    x: 100,
+    duration: 1.5,
+    ease: "power4.out",
+    scrollTrigger: {
+      trigger: "#text-creative",
+      start: "top 80%",
+      end: "bottom 00%",
+      scrub: 1,
     },
-    "-=1.9"
-  );
+  });
 
-gsap.from("#text-creative", {
-  x: 100,
-  duration: 1.5,
-  ease: "power4.out",
-  scrollTrigger: {
-    trigger: "#text-creative",
-    start: "top 80%",
-    end: "bottom 00%",
-    scrub: 1,
-  },
-});
-
-gsap.to("#text-uimotion", {
-  x: 100,
-  duration: 1.5,
-  ease: "power4.out",
-  scrollTrigger: {
-    trigger: "#text-uimotion",
-    start: "top 80%",
-    end: "bottom 00%",
-    scrub: 1,
-  },
+  gsap.to("#text-uimotion", {
+    x: 100,
+    duration: 1.5,
+    ease: "power4.out",
+    scrollTrigger: {
+      trigger: "#text-uimotion",
+      start: "top 80%",
+      end: "bottom 00%",
+      scrub: 1,
+    },
+  });
 });
--- src/js/menu-mobile.js
+++ src/js/menu-mobile.js
@@ -7,8 +7,6 @@ burger.addEventListener("click", () => {
   burger.classList.toggle("is-active");
 });
 
-
-
 // Efeito hover ripple
 var btn = document.querySelectorAll(".button-hover");
 var btnArray = Array.from(btn);
@@ -31,37 +29,58 @@ const scrollToTop = () => {
 };
 
 // FUNCIONALIDADE: Menu seguindo as sections
-window.onload = function() {
+window.onload = function () {
   const sections = document.querySelectorAll("section");
   const navLinks = document.querySelectorAll(".navbar .nav-container li a");
   function updateActiveSection() {
-      let maxVisibleArea = 0;
-      let activeSection = null;
-      sections.forEach((section)=>{
-          const visibleArea = getVisibleArea(section);
-          if (visibleArea > maxVisibleArea) {
-              maxVisibleArea = visibleArea;
-              activeSection = section.getAttribute("id");
-          }
+    let maxVisibleArea = 0;
+    let activeSection = null;
+    sections.forEach((section) => {
+      const visibleArea = getVisibleArea(section);
+      if (visibleArea > maxVisibleArea) {
+        maxVisibleArea = visibleArea;
+        activeSection = section.getAttribute("id");
       }
-      );
-      navLinks.forEach((link)=>{
-          link.classList.remove("is-active");
-          if (link.getAttribute("href") === `#${activeSection}`) {
-              link.classList.add("is-active");
-          }
+    });
+    navLinks.forEach((link) => {
+      link.classList.remove("is-active");
+      if (link.getAttribute("href") === `#${activeSection}`) {
+        link.classList.add("is-active");
       }
-      );
+    });
   }
   function getVisibleArea(element) {
-      const rect = element.getBoundingClientRect();
-      const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-      const windowWidth = window.innerWidth || document.documentElement.clientWidth;
-      const visibleHeight = Math.min(rect.bottom, windowHeight) - Math.max(rect.top, 0);
-      const visibleWidth = Math.min(rect.right, windowWidth) - Math.max(rect.left, 0);
-      return visibleHeight * visibleWidth;
+    const rect = element.getBoundingClientRect();
+    const windowHeight =
+      window.innerHeight || document.documentElement.clientHeight;
+    const windowWidth =
+      window.innerWidth || document.documentElement.clientWidth;
+    const visibleHeight =
+      Math.min(rect.bottom, windowHeight) - Math.max(rect.top, 0);
+    const visibleWidth =
+      Math.min(rect.right, windowWidth) - Math.max(rect.left, 0);
+    return visibleHeight * visibleWidth;
   }
   window.addEventListener("scroll", updateActiveSection);
   window.addEventListener("resize", updateActiveSection);
+};
+
+function myFunction(x) {
+  if (x.matches) {
+    // If media query matches
+    containerSm.classList.remove("container");
+  } else {
+    containerSm.classList.add("container");
+  }
 }
-;
+
+// Create a MediaQueryList object
+var x = window.matchMedia("(max-width: 700px)");
+var containerSm = document.querySelector(".container-sm");
+// Call listener function at run time
+myFunction(x);
+
+// Attach listener function on state changes
+x.addEventListener("change", function () {
+  myFunction(x);
+});
